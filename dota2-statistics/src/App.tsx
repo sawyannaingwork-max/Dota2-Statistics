ff2d55fa8a782979966149a1f8951662d32d9e32
sawyannaingwork-max <sawyannaingwork@gmail.com>
2026-01-10 11:37:07 +0700

    Move Header to component folder

--- dota2-statistics/src/App.tsx
+++ dota2-statistics/src/App.tsx
@@ -1,10 +1,15 @@
 import Home from "./components/Home";
+import Header from "./components/Header";
 import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+          <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+    
   )
 }
--- dota2-statistics/src/components/Home.tsx
+++ dota2-statistics/src/components/Home.tsx
@@ -1,12 +1,8 @@
-import Header from "../home/Header";
 import Content from "../home/Content";
 
 export default function Home() 
 {
     return(
-        <div>
-            <Header />
-            <Content />
-        </div>
+        <Content />
     )
 }
