98bdcda533b1a09de273cc703e97f43e73174bbb
sawyannaingwork-max <sawyannaingwork@gmail.com>
2026-01-13 14:57:17 +0700

    Making sure the nav bar is closed at the start and clicking nav link will close the nav bar

--- dota2-statistics/src/components/Header.tsx
+++ dota2-statistics/src/components/Header.tsx
@@ -4,9 +4,13 @@ import { useState } from 'react'
 
 export default function Header() 
 {
-    const [isOpen, setIsOpen] = useState<boolean>(true)
+    const [isOpen, setIsOpen] = useState<boolean>(false)
 
 
+    function handleClick()
+    {
+        setIsOpen(false)
+    }
     return(
         <header className='z-30 sticky top-0 bg-background flex justify-between items-center px-4 py-4'>
             <div className='flex gap-2 items-center'>
@@ -16,10 +20,10 @@ export default function Header()
             <nav className={`md:relative absolute top-full left-0 text-center w-full overflow-hidden bg-background duration-300 ease-linear md:py-0 md:w-auto md:h-auto ${isOpen? 'h-20 py-2' : 'h-0'}`}>
                 <ul className='md:flex gap-2'>
                     <li className='py-1'>
-                        <NavLink to="/" className='nav-link'>Home</NavLink>
+                        <NavLink onClick={handleClick} to="/" className='nav-link'>Home</NavLink>
                     </li>
                     <li className='py-1'>
-                        <NavLink to="/heroes" className='nav-link'>Heroes</NavLink>
+                        <NavLink onClick={handleClick} to="/heroes" className='nav-link'>Heroes</NavLink>
                     </li>
                 </ul>
             </nav>
