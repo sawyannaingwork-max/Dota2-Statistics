b7e7e8cf13a3e54aa0adfd5b8718998d885a495b
sawyannaingwork-max <sawyannaingwork@gmail.com>
2026-01-18 15:58:48 +0700

    As passing through state is not persistant from refresh fetching proTeam Info and passing down info through props

--- dota2-statistics/src/components/ProTeamDetail.tsx
+++ dota2-statistics/src/components/ProTeamDetail.tsx
@@ -2,22 +2,42 @@ import BasicInfo from "../proTeamDetail/BasicInfo"
 import Heroes from "../proTeamDetail/Heroes"
 import Matches from "../proTeamDetail/Matches"
 import Players from "../proTeamDetail/Players"
-import { useLocation } from "react-router-dom"
+import { useParams } from "react-router-dom"
+import useOpenDota from "../custom/useOpenDota"
+import type { ProTeam } from "../types"
 
 export default function ProTeamDetail()
 {
 
-    const location = useLocation()
+    const { id } = useParams()
 
-    const { logo_url, name } = location.state as { logo_url : string | null, name : string}
+    if (!id)
+    {
+        return <p>Invalid Usage.</p>
+    }
+
+    const { data, isFetching, isError} = useOpenDota<ProTeam>(`proteamDetail${id}`, `https://api.opendota.com/api/teams/${id}`)
+
+    if (isFetching)
+    {
+        return <p>Loading...</p>
+    }
+
+    if (isError || !data)
+    {
+        return <p>Something went wrong. Try again later.</p>
+    }
+
     return (
         <div className="bg-background min-h-screen">
-            <BasicInfo />
+            <BasicInfo 
+                {...data}
+            />
             <Players />
             <Heroes />
             <Matches 
-                logo_url = {logo_url}
-                name = {name}
+                logo_url = {data.logo_url}
+                name = {data.name}
             />
         </div>
     )
