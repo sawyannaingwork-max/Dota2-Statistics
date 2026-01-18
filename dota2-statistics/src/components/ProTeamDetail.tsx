f307f5a9c3ddc6e35e9159d061698314063d5105
sawyannaingwork-max <sawyannaingwork@gmail.com>
2026-01-18 15:24:28 +0700

    also passing team name

--- dota2-statistics/src/components/ProTeamDetail.tsx
+++ dota2-statistics/src/components/ProTeamDetail.tsx
@@ -1,9 +1,14 @@
 import BasicInfo from "../proTeamDetail/BasicInfo"
 import Heroes from "../proTeamDetail/Heroes"
 import Players from "../proTeamDetail/Players"
+import { useLocation } from "react-router-dom"
 
 export default function ProTeamDetail()
 {
+
+    const { state } = useLocation()
+
+    console.log(state)
     return (
         <div className="bg-background min-h-screen">
             <BasicInfo />
