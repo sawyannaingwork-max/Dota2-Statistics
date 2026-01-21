ab7d00afb545e0f0482fcab5f0731adfb167a426
sawyannaingwork-max <sawyannaingwork@gmail.com>
2026-01-21 14:05:02 +0700

    updating error boundary component

--- dota2-statistics/src/components/ErrorBoundary.tsx
+++ dota2-statistics/src/components/ErrorBoundary.tsx
@@ -1,31 +1,31 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import type { ReactNode } from "react";
+type Props = {
+  children: ReactNode;
+};
 
 type State = {
-    hasError : boolean
-}
+  hasError: boolean;
+};
 
-type Props = {
-    children : ReactNode
-}
 export default class ErrorBoundary extends Component<Props, State> {
-    state = { hasError : false }
+  state: State = {
+    hasError: false,
+  };
 
-    static getDerivedStateFromError()
-    {
-        return { hasError : true}
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: React.ErrorInfo) {
+    console.error("ErrorBoundary caught an error:", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong.</h2>;
     }
 
-    componentDidCatch(error : Error,  info : React.ErrorInfo) {
-        console.log(error, info)   
-    }
-
-    render() {
-        if (this.state.hasError)
-        {
-            return <h1>Something went wrong. Try again later</h1>
-        }
-
-        return this.props.children
-    }
+    return this.props.children;
+  }
 }
