bf31a1169c014922fe160f787863614af40ad134
sawyannaingwork-max <sawyannaingwork@gmail.com>
2026-01-22 13:28:23 +0700

    code splitting

--- dota2-statistics/src/App.tsx
+++ dota2-statistics/src/App.tsx
@@ -1,34 +1,41 @@
-import Home from "./components/Home";
 import Header from "./components/Header";
 import { Routes, Route } from 'react-router-dom'
-import Heroes from "./components/Heroes";
-import HeroDetail from "./components/HeroDetail";
-import ProMatchDetail from "./components/ProMatchDetail";
-import PublicMatches from "./components/PublicMatches";
-import PublicMatchDetail from "./components/PublicMatchDetail";
-import ProMatches from "./components/ProMatches";
-import Teams from "./components/Teams";
-import ProTeamDetail from "./components/ProTeamDetail";
-import PlayerDetail from "./components/PlayerDetail";
+import { lazy, Suspense } from "react";
+import Loader from "./components/Loader";
 import ErrorBoundary from "./components/ErrorBoundary";
+
+// Lazy Loading
+const Home = lazy(() => import("./components/Home"))
+const Heroes = lazy(() => import("./components/Heroes"))
+const HeroDetail = lazy(() => import("./components/HeroDetail"))
+const ProMatchDetail = lazy(() => import("./components/ProMatchDetail"))
+const PublicMatches = lazy(() => import("./components/PublicMatches"))
+const PublicMatchDetail = lazy(() => import("./components/PublicMatchDetail"))
+const ProMatches = lazy(() => import("./components/ProMatches"))
+const Teams = lazy(() => import("./components/Teams"))
+const ProTeamDetail = lazy(() => import("./components/ProTeamDetail"))
+const PlayerDetail = lazy(() => import("./components/PlayerDetail"))
+
 
 export default function App() {
   return (
     <>
     <ErrorBoundary>
         <Header />
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/heroes" element={<Heroes />} />
-            <Route path="/heroes/:id" element={<HeroDetail />} />
-            <Route path="/matches/pro/:id" element={<ProMatchDetail />} />
-            <Route path="/public" element={<PublicMatches />} />
-            <Route path="/matches/public/:id" element={<PublicMatchDetail />} />
-            <Route path="/pro" element={<ProMatches />} />
-            <Route path="/team" element={<Teams />} />
-            <Route path="/team/:id" element={<ProTeamDetail />} />
-            <Route path="player/:id" element={<PlayerDetail />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/heroes" element={<Heroes />} />
+              <Route path="/heroes/:id" element={<HeroDetail />} />
+              <Route path="/matches/pro/:id" element={<ProMatchDetail />} />
+              <Route path="/public" element={<PublicMatches />} />
+              <Route path="/matches/public/:id" element={<PublicMatchDetail />} />
+              <Route path="/pro" element={<ProMatches />} />
+              <Route path="/team" element={<Teams />} />
+              <Route path="/team/:id" element={<ProTeamDetail />} />
+              <Route path="player/:id" element={<PlayerDetail />} />
+          </Routes>
+        </Suspense>
       </ErrorBoundary>
     </>
     
