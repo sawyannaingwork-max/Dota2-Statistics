50e5dd2fb24c31b071d0446b31480c0690bb5391
sawyannaingwork-max <sawyannaingwork@gmail.com>
2026-01-23 14:28:28 +0700

    adding animation to heroes

--- dota2-statistics/src/heroes/Filter.tsx
+++ dota2-statistics/src/heroes/Filter.tsx
@@ -1,4 +1,8 @@
 import type { HeroAttribute } from "./../types";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+
 
 interface FilterProps {
     search: string;
@@ -18,14 +22,32 @@ export default function Filter({search, setSearch, filter, setFilter}: FilterPro
 
         setFilter(attribute)
     }
+
+    const timeline = gsap.timeline()
+    const containerRef = useRef<HTMLDivElement | null>(null)
+    useGSAP(() => {
+        timeline.from("#filter-hero", {
+            width : 0,
+            duration : 1,
+            opacity : 0,
+        })
+
+        timeline.from(".filter-btn", {
+            opacity : 0,
+            y : -20,
+            stagger : 0.2
+        })
+    }, {scope : containerRef, dependencies : []})
+    
     return (
-        <div className="pt-9 sticky top-15 z-10 bg-background">
+        <div ref={containerRef} className="pt-9 sticky top-15 z-10 bg-background">
             <input 
                 type="text" 
                 placeholder="Search for a hero" 
                 className="border-2 border-text rounded-sm w-[90%] mx-auto block px-2 py-1 text-text outline-none"
                 value = {search}
                 onChange={(e) => setSearch(e.target.value)}
+                id = "filter-hero"
             />
             <div className="flex gap-2 w-[90%] mx-auto pt-6">
                 <button onClick={() => handleClick("all")} className={`filter-btn ${filter === "all"? "active-btn" : ""}`}>Universal</button>
--- dota2-statistics/src/heroes/HeroList.tsx
+++ dota2-statistics/src/heroes/HeroList.tsx
@@ -2,6 +2,12 @@ import heroes from "./../helpers/heroes.json"
 import type { HeroAttribute } from "../types";
 import type { Hero } from "../types";
 import HeroCard from "./HeroCard";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger)
 
 // Type of HeroList Props
 interface HeroListProps {
@@ -11,6 +17,8 @@ interface HeroListProps {
 export default function HeroList({search, filter} : HeroListProps)
 {
     const heroListArr = Object.values(heroes) as Hero[];
+
+    const containerRef = useRef<HTMLDivElement | null>(null)
 
     // Filtering heroes to show
     // Filter according to search
@@ -29,9 +37,23 @@ export default function HeroList({search, filter} : HeroListProps)
     }
 
 
+    useGSAP(() => {
+        if (!containerRef.current)
+        {
+            return
+        }
+        
+        gsap.from(containerRef.current, {
+            y : 30,
+            duration : 2,
+            opacity : 0,
+            ease : "elastic"
+        })
+    })
+
     // Show a list of hero
     return(
-        <div className="w-[90%] mx-auto mt-9 grid grid-cols-1 sm:justify-between sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+        <div ref={containerRef} className="overflow-x-hidden w-[90%] mx-auto mt-9 grid grid-cols-1 sm:justify-between sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
             {
                 filteredHeroes?.map(function(hero)
                 {
