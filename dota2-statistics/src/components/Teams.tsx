cde5e82d3bc4df8251ace759bc1881848f1cdab5
sawyannaingwork-max <sawyannaingwork@gmail.com>
2026-01-24 12:19:44 +0700

    Adding animation to pro team list

--- dota2-statistics/src/components/Teams.tsx
+++ dota2-statistics/src/components/Teams.tsx
@@ -1,13 +1,40 @@
 import useOpenDota from "../custom/useOpenDota"
 import type { ProTeam } from "../types"
 import Team from "./../proTeams/Team"
-import { useState } from "react"
+import { use, useRef, useState } from "react"
 import Loader from "./Loader"
+
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+
 
 export default function Teams()
 {
     const [search, setSearch] = useState<string>()
+
+    const elementRef = useRef<HTMLDivElement | null>(null)
+
     let { data : teams, isFetching , isError} = useOpenDota<ProTeam[]>("proteam", "https://api.opendota.com/api/teams")
+
+    useGSAP(() => {
+
+        const tl = gsap.timeline()
+
+        tl.from("#team", {
+            width : 0,
+            opacity : 0,
+            ease : "sine",
+            duration : 1
+        })
+
+        tl.from("#team-container", {
+            opacity : 0,
+            y : 20,
+            ease : "sine"
+        })
+
+    }, { scope : elementRef, dependencies : [isFetching]})
+
 
     if (isFetching)
     {
@@ -38,9 +65,9 @@ export default function Teams()
     })
 
     return(
-        <div className="bg-background pt-9 min-h-screen">
+        <div ref={elementRef} className="bg-background pt-9 min-h-screen">
             <input className="outline-none w-[90%] border-2 mx-auto block border-text rounded-md py-1 text-text px-2 " type="text" name="team" id="team" placeholder="Search Team" value={search} onChange={(e) => setSearch(e.target.value)} />
-            <div className="w-[90%] mt-9 mx-auto grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+            <div id="team-container" className="w-[90%] mt-9 mx-auto grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
                 {teamArr}
             </div>
         </div>
