fef5b4018d6706d1ae8986a2a7d49e2d7604782e
sawyannaingwork-max <sawyannaingwork@gmail.com>
2026-01-24 12:04:38 +0700

    Adding animation to pro matches

--- dota2-statistics/src/components/ProMatches.tsx
+++ dota2-statistics/src/components/ProMatches.tsx
@@ -3,10 +3,41 @@ import type { ProTeamMatch } from "../types"
 import Match from "./../proMatches/Match"
 import Loader from "./Loader"
 
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+import { ScrollTrigger } from "gsap/all"
+import { useRef } from "react"
+
+gsap.registerPlugin(ScrollTrigger)
+
 
 export default function ProMatches()
 {
     const { data : matches, isFetching, isError } = useOpenDota<ProTeamMatch[]>("promatches", "https://api.opendota.com/api/proMatches")
+
+    const elementRef = useRef<HTMLDivElement | null>(null)
+
+    useGSAP(() => {
+        if (!elementRef.current)
+        {
+            return
+        }
+
+        const matches : HTMLElement[] = gsap.utils.toArray(elementRef.current.children)
+
+        matches.forEach(match => {
+            gsap.from(match, {
+                opacity : 0,
+                x : -20,
+                duration : 1,
+                ease : "sine",
+                scrollTrigger : {
+                    trigger : match,
+                    start : "bottom bottom"
+                }
+            })
+        })
+    }, {scope : elementRef, dependencies : [isFetching]})
 
     if (isFetching)
     {
@@ -30,7 +61,7 @@ export default function ProMatches()
     })
 
     return(
-        <div className="bg-background py-9">
+        <div ref={elementRef} className="bg-background py-9">
             {matchesArr}
         </div>
     )
