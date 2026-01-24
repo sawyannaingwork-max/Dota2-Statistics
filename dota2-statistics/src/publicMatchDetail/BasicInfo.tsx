4c53f3eeb3e2b9eb22e30caf6a83ea611951d798
sawyannaingwork-max <sawyannaingwork@gmail.com>
2026-01-24 15:07:15 +0700

    Adding animation to public match detail

--- dota2-statistics/src/publicMatchDetail/BasicInfo.tsx
+++ dota2-statistics/src/publicMatchDetail/BasicInfo.tsx
@@ -1,25 +1,81 @@
 import { usePublicMatchContext } from "../components/PublicMatchDetail"
+import gsap from "gsap"
+import { SplitText } from "gsap/all"
+gsap.registerPlugin(SplitText)
+import { useGSAP } from "@gsap/react"
+import { useRef } from "react"
 
 export default function BasicInfo()
 {
     const data = usePublicMatchContext()
     const date = new Date(data.start_time * 1000)
 
+    const elementRef = useRef<HTMLDivElement | null>(null)
+
+    useGSAP(() => {
+
+        if (!elementRef.current)
+        {
+            return
+        }
+
+        const timeline = gsap.timeline()
+        const split = new SplitText("#duration", {
+            type : "words"
+        })
+
+        timeline.from("#winner-info", {
+            opacity : 0,
+            y : -20,
+            duration : 0.6,
+            ease : "sine"
+        })
+
+        timeline.from(split.words, {
+            opacity : 0,
+            stagger : 0.2,
+            ease : "sine"
+        })
+
+        timeline.from("#radiant", {
+            x : -20,
+            opacity : 0,
+            duration : 0.6,
+            ease : "sine"
+        })
+
+        timeline.from("#vs", {
+            opacity : 0,
+            scale : 0,
+            duration : 0.6,
+            ease : "sine"
+        })
+
+        timeline.from("#dire", {
+            x : 20,
+            opacity : 0,
+            duration : 0.6,
+            ease : "sine"
+        })
+
+        return(() => timeline.kill())
+
+    }, { scope : elementRef, dependencies : []})
 
     return (
-        <div className="w-[90%] mx-auto pt-9">
-            <div className="flex justify-between lg:justify-start">
-                <h3 className="text-xl font-inter text-text">{data.radiant_win? "Radiant Victory" : "Dire Victory"}</h3>
+        <div ref={elementRef} className="w-[90%] mx-auto pt-9">
+            <div id="winner-info" className="flex justify-between lg:justify-start">
+                <h3  className="text-xl font-inter text-text">{data.radiant_win? "Radiant Victory" : "Dire Victory"}</h3>
                 <h3 className="text-accent">{date.getDate()}/ {date.getMonth() + 1}/ {date.getFullYear()}</h3>
             </div>  
-            <p className="mt-3 text-secondary">Duration : <span className="text-text">{String(Math.floor(data.duration / 60)).padStart(2, "0")} : {String(data.duration % 60).padStart(2, "0")}</span></p>
+            <p id="duration" className="mt-3 text-secondary">Duration : <span className="text-text">{String(Math.floor(data.duration / 60)).padStart(2, "0")} : {String(data.duration % 60).padStart(2, "0")}</span></p>
             <div className="flex justify-between items-center mt-5">
-                <div className="flex items-center gap-5">
+                <div id="radiant" className="flex items-center gap-5">
                     <h2 className="text-green-400">Radiant</h2>
                     <span className="text-xl text-text font-itim">{data.radiant_score}</span>
                 </div>
-                <div className="text-2xl text-text font-itim">VS</div>
-                <div className="flex items-center gap-5">
+                <div id="vs" className="text-2xl text-text font-itim">VS</div>
+                <div id="dire" className="flex items-center gap-5">
                     <span className="text-xl text-text font-itim">{data.dire_score}</span>
                     <h2 className="text-red-500">Dire</h2>
                 </div>
--- dota2-statistics/src/publicMatchDetail/Data.tsx
+++ dota2-statistics/src/publicMatchDetail/Data.tsx
@@ -1,15 +1,34 @@
+import { useRef } from "react"
 import { usePublicMatchContext } from "../components/PublicMatchDetail"
 import Table from "./Table"
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
 
 export default function Data()
 {
     const data = usePublicMatchContext()
+    const elementRef = useRef<HTMLDivElement | null>(null)
 
     const radiant = data.players.slice(0, data.players.length / 2)
     const dire = data.players.slice(data.players.length / 2)
     
+    useGSAP(() => {
+        if (!elementRef.current)
+        {
+            return 
+        }
+
+        gsap.from(elementRef.current, {
+            y : 20,
+            opacity : 0,
+            duration : 0.6,
+            delay : 3,
+            ease : "sine"
+        })
+
+    }, { scope : elementRef, dependencies : []})
     return(
-        <div className="w-[90%] mx-auto mt-9">
+        <div ref={elementRef} className="w-[90%] mx-auto mt-9">
             <div>
                 <h2 className="text-green-400 text-2xl">Radiant</h2>
                 <Table 
