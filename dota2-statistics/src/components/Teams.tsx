1c97648c3a415c8dd51452ae51a4917d0b88a5f6
sawyannaingwork-max <sawyannaingwork@gmail.com>
2026-01-25 16:10:35 +0700

    Adding skeleton loader to pro team list page

--- dota2-statistics/src/components/Teams.tsx
+++ dota2-statistics/src/components/Teams.tsx
@@ -1,8 +1,7 @@
 import useOpenDota from "../custom/useOpenDota"
 import type { ProTeam } from "../types"
 import Team from "./../proTeams/Team"
-import { use, useRef, useState } from "react"
-import Loader from "./Loader"
+import { useRef, useState } from "react"
 
 import gsap from "gsap"
 import { useGSAP } from "@gsap/react"
@@ -38,7 +37,63 @@ export default function Teams()
 
     if (isFetching)
     {
-        return <Loader />
+        return (
+            <>
+            {Array.from({ length: 8 }).map((_, i) => (
+                <div
+                key={i}
+                className="
+                    w-[90%]
+                    m-5
+                    mx-auto
+                    rounded-md
+                    flex flex-col items-center
+                    shadow-md shadow-blue-400
+                    animate-pulse
+                    px-2 py-3
+                "
+                >
+                {/* Team name */}
+                <div className="h-7 w-44 mb-2 bg-gray-700 rounded" />
+
+                {/* Logo */}
+                <div className="w-40 h-40 bg-gray-700 rounded mb-2" />
+
+                {/* Table */}
+                <table>
+                    <tbody>
+                    <tr>
+                        <th className="px-5 py-1">
+                        <div className="h-4 w-14 bg-gray-700 rounded" />
+                        </th>
+                        <td>
+                        <div className="h-4 w-12 bg-gray-700 rounded" />
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th className="px-5 py-1">
+                        <div className="h-4 w-12 bg-gray-700 rounded" />
+                        </th>
+                        <td>
+                        <div className="h-4 w-10 bg-gray-700 rounded" />
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th className="px-5 py-1">
+                        <div className="h-4 w-14 bg-gray-700 rounded" />
+                        </th>
+                        <td>
+                        <div className="h-4 w-10 bg-gray-700 rounded" />
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                </div>
+            ))}
+            </>
+        )
     }
 
     if (isError || !teams)
